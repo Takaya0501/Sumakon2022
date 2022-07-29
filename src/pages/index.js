29c361fcf792a0a0472097557b294d790af16982
TakayaOkumura <19210@toba.kosen-ac.jp>
2022-07-29 19:17:45 +0900

    0729 commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
 import { AiFillCamera } from 'react-icons/ai';
 import { MdPhotoLibrary } from 'react-icons/md';
-// import { IconContext } from 'react-icons';
 
-export default function ProductsList() {
+export default function posting() {
+  const [preview, setPreview] = useState('');
+
+  const handleChangeFile = (e) => {
+    const { files } = e.target;
+    setPreview(window.URL.createObjectURL(files[0]));
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,15 +21,23 @@ export default function ProductsList() {
 
       <main>
         <div className={styles.iconCircle} />
-        <img src="/image1.png" />
+        {/* <div className={preview} */}
+        <button onClick={preview}>ファイルアップロード</button>
+        <input type="file" accept="image/*" name="photo" onChange={handleChangeFile} />
+        <br />
+        <img src={preview} name="aaa" width="20%" height="20%" />
       </main>
-      {/* <p className={styles.iconpicture}>
-        <img src="/../../picture/camera.jpeg" alt="表示できません" />
-      </p> */}
-      <h1>
-        <MdPhotoLibrary className={styles.MdPhotoLibrary} />
-        <AiFillCamera className={styles.AiFillCamera} />
-      </h1>
+      {/* <div className={styles.imageContainer}>
+        <Image className={styles.image} src="/picture/image1.png" width="200%" height="200%" />
+      </div> */}
+
+      <form action="/send-data-here" method="post">
+        <label for="first">投稿文章</label>
+        <br />
+        <input type="text" id="text" name="text" />
+        <br />
+        <button type="submit">投稿</button>
+      </form>
     </div>
   );
 }
