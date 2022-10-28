9c0a059ab9662a53b27462e08bc39843f2228259
TakayaOkumura <19210@toba.kosen-ac.jp>
2022-10-28 10:42:28 +0900

    いいね写真変更

--- pages/posts/[post_id].js
+++ pages/posts/[post_id].js
@@ -110,7 +110,7 @@ export default function Postview(props) {
     getComments();
   }, [post_id]);
 
-  //いいね
+  //いいね写真
   var pics_src = new Array("/image/good.png", "/image/redgood.png");
   var num = 0;
   function slideshow() {
@@ -184,7 +184,7 @@ export default function Postview(props) {
                           <img
                             id="mypic"
                             onClick={slideshow}
-                            src={"/image/good.png"}
+                            src={"/image/1.png"}
                             width="100"
                             height="100"
                           />
